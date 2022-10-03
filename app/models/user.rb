be009f9f54301e163b0b10b24eec2a43b65a1e76
ian gottheim <iangottheim@gmail.com>
2022-10-03 12:20:45 -0400

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,19 @@
+
+ require 'bcrypt'
+
 class User < ActiveRecord::Base
     has_many :tasks
     has_many :categories, through: :tasks
+    
+    # users.password_hash in the database is a :string
+#   include BCrypt
 
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
 end
